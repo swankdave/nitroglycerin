b557493e5f689eef90700f06790cada92d12caf6
Caleb Veenstra <caleb.veenstra@icademystudents.org>
2020-01-23 19:32:25 -0500

    init commit of nitroglycerin

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,31 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants.  This class should not be used for any other purpose.  All constants should be
+ * declared globally (i.e. public static).  Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+    public static final class drivetrain {
+        public static final int LEFT_MASTER_ID = 10;
+        public static final int RIGHT_MASTER_ID = 11;
+        public static final int LEFT_SLAVE_ID = 12;
+        public static final int RIGHT_SLAVE_ID = 13;
+        public static final double SPEED_MULTIPLIER = 1.0;
+    }
+
+    public static final class joystick {
+        public static final int LEFT_JOY_ID = 0;
+        public static final int RIGHT_JOY_ID = 1;
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -0,0 +1,56 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import frc.robot.Constants.drivetrain;
+
+import static java.lang.Math.abs;
+import static java.lang.Math.pow;
+
+
+public class DrivetrainSubsystem extends SubsystemBase {
+
+  private CANSparkMax left_master = new CANSparkMax(drivetrain.LEFT_MASTER_ID, MotorType.kBrushless);
+  private CANSparkMax left_slave = new CANSparkMax(drivetrain.LEFT_SLAVE_ID, MotorType.kBrushless);
+  private CANSparkMax right_master = new CANSparkMax(drivetrain.RIGHT_MASTER_ID, MotorType.kBrushless);
+  private CANSparkMax right_slave = new CANSparkMax(drivetrain.RIGHT_SLAVE_ID, MotorType.kBrushless);
+
+
+  public DrivetrainSubsystem() {
+    left_master.setIdleMode(IdleMode.kBrake);
+    left_slave.setIdleMode(IdleMode.kBrake);
+    right_master.setIdleMode(IdleMode.kBrake);
+    right_slave.setIdleMode(IdleMode.kBrake);
+
+    left_master.setOpenLoopRampRate(1);
+    left_slave.setOpenLoopRampRate(1);
+    right_master.setOpenLoopRampRate(1);
+    right_slave.setOpenLoopRampRate(1);
+
+    left_slave.follow(left_master);
+    right_slave.follow(right_master);
+
+  }
+
+  public void drive(double left_speed, double right_speed) {
+    left_master.set(left_speed);
+    right_master.set(-right_speed);
+  }
+
+  public double deadband_handler(double speed) {
+    if (abs(speed) > 0.1) {
+      return speed;
+    } else {
+      return 0.0;
+    }
+  }
+}
