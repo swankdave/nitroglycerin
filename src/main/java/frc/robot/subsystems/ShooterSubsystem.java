caa66c2a532a364d04f50984e5eae805184e2f9c
Caleb Veenstra <caleb.veenstra@icademystudents.org>
2020-01-28 20:13:05 -0500

    Revised Shootersubsystem.java to use PID constants to run at a given rpm. Testing needed to determine native rmp to acutal rpm.

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -14,9 +14,19 @@ public class ShooterSubsystem extends SubsystemBase {
 
 
     public ShooterSubsystem() {
-        //Add any init code here, pid values etc
+        //Set Brake neutral mode.
         top_shooter_motor.setNeutralMode(NeutralMode.Brake);
         bottom_shooter_motor.setNeutralMode(NeutralMode.Brake);
+        //Top shooter motor pid configs
+        top_shooter_motor.selectProfileSlot(0, 0);
+        top_shooter_motor.config_kP(0, Constants.shooter.pid_constants.top_motor.P);
+        top_shooter_motor.config_kI(0, Constants.shooter.pid_constants.top_motor.I);
+        top_shooter_motor.config_kD(0, Constants.shooter.pid_constants.top_motor.D);
+        //Bottom shooter motor pid configs
+        bottom_shooter_motor.selectProfileSlot(0, 0);
+        bottom_shooter_motor.config_kP(0, Constants.shooter.pid_constants.bottom_motor.P);
+        bottom_shooter_motor.config_kI(0, Constants.shooter.pid_constants.bottom_motor.I);
+        bottom_shooter_motor.config_kD(0, Constants.shooter.pid_constants.bottom_motor.D);
     }
 
     public void test_shooter() {
@@ -31,5 +41,16 @@ public class ShooterSubsystem extends SubsystemBase {
         SmartDashboard.putNumber("Top Shooter Output Current: (amps)", top_shooter_motor.getStatorCurrent());
         SmartDashboard.putNumber("Bottom Shooter Output Current: (amps)", bottom_shooter_motor.getStatorCurrent());
         SmartDashboard.putNumber("Bottom Shooter Velocity: ", bottom_shooter_motor.getSelectedSensorVelocity());
+    }
+
+    public void run_at_rpm(int rpm) {
+        top_shooter_motor.set(TalonFXControlMode.Velocity, -rpm);
+        bottom_shooter_motor.set(TalonFXControlMode.Velocity, rpm);
+    }
+
+    public void shooter_neutral() {
+        top_shooter_motor.neutralOutput();
+        bottom_shooter_motor.neutralOutput();
+
     }
 }
